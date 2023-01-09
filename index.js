38907968b28e70dcbb685fe11f8c434fba8b7dbf
chadwithz <kepoya@email.com>
2023-01-09 18:06:13 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import { Client } from 'whatsapp-web.js'
+import qrcode from 'qrcode-terminal'
+
+const client = new Client({
+    puppeteer: {
+		args: ['--no-sandbox'],
+	}
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async msg => {
+    const chat = await msg.getChat()
+    const isGroup = chat.isGroup
+    // ignores if from group
+    if(!isGroup){
+        if(msg.hasMedia) {
+            const sticker = await msg.downloadMedia()
+            client.sendMessage(msg.from, sticker, { sendMediaAsSticker: true})
+        }
+    }
+});
+
+client.initialize();
